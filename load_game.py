6f91a2b9f8f49b5cd0b667082c098b7b153968fc
Espelet <espeletpro@gmaiil.com>
2023-03-23 12:43:58 +0100

    génération terrain dans la console

--- load_game.py
+++ load_game.py
@@ -1,21 +1,35 @@
 from numpy import random as rd
 import field_constructor as fld
+from pynput import keyboard
+
 
 class jeu(list):
     def __init__(self, niveau):
-        self.nom_niveau, self.largeur, self.hauteur, self.nombre_pierre, self.nombre_diamant = self.carac_terrain(niveau)
+        self.niv = niveau
+        self.nom_niveau, self.largeur, self.hauteur, self.nombre_pierre, \
+        self.nombre_diamant = self.carac_terrain(self.niv)
         for k in range(self.nombre_pierre - 1):
-            self.append(fld.Pierre(rd.randint(0, self.largeur),rd.randint(0, self.hauteur)))
+            self.append(fld.Pierre(rd.randint(1, self.largeur - 2), rd.randint(1, self.hauteur - 2), jeu))
         for l in range(self.nombre_diamant - 1):
-            self.append(fld.Diamant(rd.randint(0, self.largeur),rd.randint(0, self.hauteur)))
-
+            self.append(fld.Diamant(rd.randint(1, self.largeur - 2), rd.randint(1, self.hauteur - 2), jeu))
 
     def __str__(self):
-        res = " "
-        for i in range(self.largeur - 1):
-            for j in range(self.hauteur - 1):
-                for k in range(self.nombre_pierre):
-
+        a = ""
+        res = "Niveau " + self.nom_niveau + "\n"
+        for i in range(self.largeur):
+            for j in range(self.hauteur):
+                if j == 0 or j == self.hauteur - 1 or i == 0 or i == self.largeur - 1:
+                    a = fld.Brique(i, j, jeu).car()
+                else:
+                    for obj in self:
+                        if obj.coords == (i, j):
+                            a = str(obj.car())
+                            break
+                        else:
+                            a = " "
+                res += a + " "
+            res += "\n"
+        return res
 
     def carac_terrain(self, niveau):
         nom_niveau = "A" + str(niveau)
@@ -31,3 +45,7 @@ class jeu(list):
             nbre_diamant = 10
         return nom_niveau, largeur, hauteur, nbre_pierre, nbre_diamant
 
+
+if __name__ == "__main__":
+    j = jeu(1)
+    print(j)
