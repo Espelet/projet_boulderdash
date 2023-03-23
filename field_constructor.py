cf2413bbc49d6d33d421b71dfffdcc4de577c17c
Espelet <espeletpro@gmaiil.com>
2023-03-23 12:10:46 +0100

    23/03/2023

--- field_constructor.py
+++ field_constructor.py
@@ -1,46 +1,77 @@
 from load_game import *
 
-class terrain(jeu):
-    def __init__(self, x, y):
+class terrain:
+    def __init__(self, x, y, jeu):
+        self.jeu = jeu
         self.a = 12
-        self.x = x
-        self.y = y
+        self.__coords = (x, y)
+
+    @property
+    def coords(self):
+        return self.__coords
+
+    @property
+    def x(self):
+        return self.__coords[0]
+
+    @property
+    def y(self):
+        return self.__coords[1]
+
+    @coords.setter
+    def coords(self, val):
+        x, y = val
+        x = max(min(x, self.jeu.largeur), 0)
+        y = max(min(y, self.jeu.hauteur), 0)
+        self.__coords = (x, y)
 
 
 class Joueur(terrain):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, jeu):
+        super().__init__(x, y, jeu)
         self.SeDeplace = True
         self.score = 0
         self.vie = 3
 
 
 class Diamant(terrain):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, jeu):
+        super().__init__(x, y, jeu)
         self.val = 1
         self.EstConso = True
         self.EstImmobile = False
 
+    def car(self):
+        return "D"
+
 
 class Pierre(terrain):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, jeu):
+        super().__init__(x, y, jeu)
         self.Gravite = True
         self.EstImmobile = False
         self.EstConso = False
         self.damage = 1
 
+    def car(self):
+        return "P"
+
 
 class Brique(terrain):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, jeu):
+        super().__init__(x, y, jeu)
         self.EstImmobile = True
         self.EstConso = False
 
+    def car(self):
+        return "B"
+
 
 class Terre(terrain):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, jeu):
+        super().__init__(x, y, jeu)
         self.EstImmobile = True
         self.EstConso = True
+
+    def car(self):
+        return "T"
--- load_game.py
+++ load_game.py
@@ -1,6 +1,21 @@
+from numpy import random as rd
+import field_constructor as fld
+
 class jeu(list):
     def __init__(self, niveau):
-        nom_niveau, largeur, hauteur, nombre_pierre, nombre_diamant = self.carac_terrain(niveau)
+        self.nom_niveau, self.largeur, self.hauteur, self.nombre_pierre, self.nombre_diamant = self.carac_terrain(niveau)
+        for k in range(self.nombre_pierre - 1):
+            self.append(fld.Pierre(rd.randint(0, self.largeur),rd.randint(0, self.hauteur)))
+        for l in range(self.nombre_diamant - 1):
+            self.append(fld.Diamant(rd.randint(0, self.largeur),rd.randint(0, self.hauteur)))
+
+
+    def __str__(self):
+        res = " "
+        for i in range(self.largeur - 1):
+            for j in range(self.hauteur - 1):
+                for k in range(self.nombre_pierre):
+
 
     def carac_terrain(self, niveau):
         nom_niveau = "A" + str(niveau)
@@ -15,3 +30,4 @@ class jeu(list):
             nbre_pierre = 25
             nbre_diamant = 10
         return nom_niveau, largeur, hauteur, nbre_pierre, nbre_diamant
+
