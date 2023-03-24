d929f543e8c37381580e6764907179b629735967
Espelet <espeletpro@gmaiil.com>
2023-03-24 21:52:13 +0100

    gravité parfaite, ajout sortie

--- element.py
+++ element.py
@@ -1,11 +1,13 @@
 import random
 
+
 class Element:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Brick(Element):
+
+class Brique(Element):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.is_gravity_affected = False
@@ -13,6 +15,17 @@ class Brick(Element):
         self.is_consumable = False
         self.is_Falling = False
         self.symbol = "#"
+
+
+class Sortie(Element):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.is_gravity_affected = False
+        self.is_pushable = False
+        self.is_consumable = False
+        self.is_Falling = False
+        self.symbol = "S"
+
 
 class Player(Element):
     def __init__(self, x, y):
@@ -23,7 +36,8 @@ class Player(Element):
         self.is_Falling = False
         self.symbol = "P"
 
-class Diamond(Element):
+
+class Diamant(Element):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.is_gravity_affected = True
@@ -32,7 +46,8 @@ class Diamond(Element):
         self.is_Falling = False
         self.symbol = "D"
 
-class Stone(Element):
+
+class Pierre(Element):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.is_gravity_affected = True
@@ -41,7 +56,8 @@ class Stone(Element):
         self.is_Falling = False
         self.symbol = "O"
 
-class Dirt(Element):
+
+class Terre(Element):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.is_gravity_affected = False
@@ -49,6 +65,7 @@ class Dirt(Element):
         self.is_consumable = True
         self.is_Falling = False
         self.symbol = "."
+
 
 class Plateau:
     def __init__(self, width, height):
@@ -82,43 +99,63 @@ class Plateau:
         adjacent_elements = []
         for x in range(self.width):
             for y in range(self.height):
-                if self.is_valid_position(x, y) and isinstance(self.grid[x][y], (Diamond, Stone)):
+                if self.is_valid_position(x, y) and isinstance(self.grid[x][y], (Diamant, Pierre)):
                     adjacent_elements.append(self.grid[x][y])
         return adjacent_elements
 
     def move_player(self, x_offset, y_offset):
         x = self.player.x + x_offset
         y = self.player.y + y_offset
+
         if not self.is_valid_position(x, y):
             return False
+
         target_element = self.grid[x][y]
-        if isinstance(target_element, Brick):
+
+        if isinstance(target_element, Brique):
             return False
-        if isinstance(target_element, Diamond):
-            self.score += 1
+
+        elif isinstance(target_element, Diamant):
+            self.score += 10
             self.remove_element(target_element)
             self.move_element(self.player, x, y)
             return self.score
-        if isinstance(target_element, Stone):
-            if y_offset == -1 or not self.move_element(target_element, x, y) \
-                    or not self.is_valid_position(target_element.x + x_offset, target_element.y + y):
-                return False
 
+        elif isinstance(target_element, Sortie):
+            self.remove_element(self.player)
+            self.player = None
+            return True
+
+        elif isinstance(target_element, Pierre):
+            if y_offset == -1 or not self.is_valid_position(target_element.x + x_offset, target_element.y + y) \
+                    or self.is_used(target_element.x + x_offset, target_element.y + y_offset):
+                return False
+            else:
+                self.move_element(target_element, target_element.x + x_offset, target_element.y + y_offset)
+                self.move_element(self.player, x, y)
+                return True
         self.move_element(self.player, x, y)
-        return True
 
     def apply_gravity(self, el):
         if isinstance(self.grid[el.x][el.y + 1], Player) and el.is_Falling:
+            self.remove_element(self.player)
+            self.move_element(el, el.x, el.y + 1)
+            self.player = None
             return True
-        if isinstance(el, (Diamond, Stone)):
+        if isinstance(el, (Diamant, Pierre)):
             if el.y + 1 < self.height:
                 if self.grid[el.x][el.y + 1] is None:
                     el.is_Falling = True
                     self.move_element(el, el.x, el.y + 1)
                     self.apply_gravity(el)
-                else:
-                    el.is_Falling = False
+                elif isinstance(self.grid[el.x][el.y + 1], (Diamant, Pierre)):
+                    if self.grid[el.x + 1][el.y + 1] is None and self.grid[el.x + 1][el.y] is None:
+                        el.is_Falling = True
+                        self.move_element(el, el.x + 1, el.y)
+                        self.apply_gravity(el)
+                    elif self.grid[el.x - 1][el.y + 1] is None and self.grid[el.x - 1][el.y] is None:
+                        el.is_Falling = True
+                        self.move_element(el, el.x - 1, el.y)
+                        self.apply_gravity(el)
             else:
                 el.is_Falling = False
-
-
--- jeu.py
+++ jeu.py
@@ -5,8 +5,9 @@ from element import *
 import numpy.random as rd
 import keyboard
 
+
 class BoulderDash:
-    def __init__(self, width, height, nbr_diams,nbr_pierre, nbr_brick):
+    def __init__(self, width, height, nbr_diams, nbr_pierre, nbr_brick):
         self.P = Plateau(width, height)
         self.grid = self.P.grid
         self.height = height
@@ -36,14 +37,15 @@ class BoulderDash:
         for j in range(self.height):
             for i in range(self.width):
                 if j == 0 or j == self.height - 1 or i == 0 or i == self.width - 1:
-                    self.P.add_element(Brick(i, j))
+                    self.P.add_element(Brique(i, j))
                 else:
                     pass
-        self.ajt_element(Diamond, self.nbr_diams)
-        self.ajt_element(Brick, self.nbr_brick)
-        self.ajt_element(Stone, self.nbr_pierre)
-        nbr_dirt = ((self.height - 2) * (self.width - 2) - (self.nbr_pierre + self.nbr_brick + self.nbr_diams))//(10/7)
-        self.ajt_element(Dirt, nbr_dirt)
+        self.ajt_element(Diamant, self.nbr_diams)
+        self.ajt_element(Brique, self.nbr_brick)
+        self.ajt_element(Pierre, self.nbr_pierre)
+        nbr_dirt = ((self.height - 2) * (self.width - 2) - (self.nbr_pierre + self.nbr_brick + self.nbr_diams)) // (
+                    10 / 7)
+        self.ajt_element(Terre, nbr_dirt)
         player_pos = (0, 0)
         while self.P.is_used(player_pos[0], player_pos[1]):
             player_pos = (random.randint(0, self.width - 1), random.randint(0, self.height - 1))
@@ -51,48 +53,74 @@ class BoulderDash:
         self.P.add_element(player)
         self.falling_ent = self.P.get_falling_elements()
 
-
-
     def ajt_element(self, element, n):
         if n == 0:
             return True
         else:
             coords = rd.randint(1, self.width), rd.randint(1, self.height)
-            if not self.P.is_used(coords[0], coords[1]):
+            print(self.P.is_used(coords[0], coords[1]))
+            if not self.P.is_used(coords[0], coords[1]) or isinstance(element, Sortie):
                 self.P.add_element(element(coords[0], coords[1]))
                 return self.ajt_element(element, n - 1)
             else:
                 return self.ajt_element(element, n)
+
     def move(self):
+        down_pressed, up_pressed, left_pressed, right_pressed = False, False, False, False
+
         while True:
             if self.update():
                 break
-            if keyboard.is_pressed("down arrow"):
+
+            if keyboard.is_pressed("down arrow") and not down_pressed:
                 self.P.move_player(0, 1)
+                down_pressed = True
                 print(self)
-            if keyboard.is_pressed("up arrow"):
+            elif not keyboard.is_pressed("down arrow"):
+                down_pressed = False
+
+            if keyboard.is_pressed("up arrow") and not up_pressed:
                 self.P.move_player(0, -1)
                 print(self)
-            if keyboard.is_pressed("left arrow"):
+                up_pressed = True
+            elif not keyboard.is_pressed("up arrow"):
+                up_pressed = False
+
+            if keyboard.is_pressed("left arrow") and not left_pressed:
                 self.P.move_player(-1, 0)
                 print(self)
-            if keyboard.is_pressed("right arrow"):
+                left_pressed = True
+            elif not keyboard.is_pressed("left arrow"):
+                left_pressed = False
+
+            if keyboard.is_pressed("right arrow") and not right_pressed:
                 self.P.move_player(1, 0)
                 print(self)
+                right_pressed = True
+            elif not keyboard.is_pressed("right arrow"):
+                right_pressed = False
+
             if keyboard.is_pressed("esc"):
                 break
 
-            time.sleep(0.033)
-
     def update(self):
+        if self.P.player is None:
+            print(self)
+            print("fin de la partie, bravo !")
+            print("score", self.P.score)
+            return True
+
         self.falling_ent = self.P.get_falling_elements()
+        if not any(isinstance(x, Diamant) for x in self.falling_ent):
+            self.ajt_element(Sortie, 1)
+
         for el in self.falling_ent:
             t = self.P.apply_gravity(el)
             if t:
                 print(self)
                 print("fin des haricots !")
+                print("score", self.P.score)
                 return True
-
 
 
 class BoulderDashTest:
@@ -106,12 +134,14 @@ class BoulderDashTest:
     def niveau_a1(self):
         width = 10
         height = 10
-        nbr_diams = 5
-        nbr_pierre = 15
+        nbr_diams = 1
+        nbr_pierre = 1
         nbr_brick = 5
         return width, height, nbr_diams, nbr_pierre, nbr_brick
+
     def run_tests(self):
         self.test_generate()
+
 
 if __name__ == "__main__":
     BoulderDashTest().run_tests()
